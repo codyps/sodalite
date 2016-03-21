92056fab8579a02ea020483fad3587a7ea38b594
Cody P Schafer <dev@codyps.com>
2016-03-21 15:12:19 -0400

    ci/doc-upload: allow multiple archs, adjust key path

--- ci/travis-doc-upload.sh
+++ ci/travis-doc-upload.sh
@@ -3,9 +3,11 @@
 # License: CC0 1.0 Universal
 # https://creativecommons.org/publicdomain/zero/1.0/legalcode
 
-set -e
+set -ex
 
-. ./ci/travis-doc-upload.cfg
+D="$(dirname "$0")"
+
+. "$D/travis-doc-upload.cfg"
 
 [ "$TRAVIS_BRANCH" = master ]
 
@@ -15,16 +17,17 @@ eval key=\$encrypted_${SSH_KEY_TRAVIS_ID}_key
 eval iv=\$encrypted_${SSH_KEY_TRAVIS_ID}_iv
 
 mkdir -p ~/.ssh
-openssl aes-256-cbc -K $key -iv $iv -in ci/id_rsa.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K "$key" -iv "$iv" -in "$D/docs_github_id.enc" -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 
-git clone --branch gh-pages git@github.com:$DOCS_REPO deploy_docs
+git clone --branch gh-pages "git@github.com:$DOCS_REPO" deploy_docs
 
 cd deploy_docs
 git config user.name "doc upload bot"
 git config user.email "nobody@example.com"
-rm -rf $PROJECT_NAME
-mv ../target/doc $PROJECT_NAME
-git add -A $PROJECT_NAME
+rm -rf "$PROJECT_NAME"
+mkdir -p "$(dirname "$PROJECT_NAME")"
+mv ../target/doc "$PROJECT_NAME"
+git add -A "$PROJECT_NAME"
 git commit -qm "doc upload for $PROJECT_NAME ($TRAVIS_REPO_SLUG)"
 git push -q origin gh-pages
