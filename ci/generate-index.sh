63aef6679ca4b9558861b90ff20e6ef30e5dedd0
Cody P Schafer <dev@codyps.com>
2016-03-28 13:26:34 -0400

    ci/generate-index: suffix support, only use dirs

--- ci/generate-index.sh
+++ ci/generate-index.sh
@@ -2,19 +2,23 @@
 
 set -eu
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <path-to-index>"
+if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <path-to-index> [<suffix>]"
   exit 1
 fi
 
+suffix="${2:index.html}"
 o="$1/.tmp_index_$$"
 rm -f "$o"
 echo '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"></meta></head><body><ul>' >> "$o"
 
 for i in "$1"/*; do
+  if ! [ -d "$i" ]; then
+    continue;
+  fi
   # TODO: proper encoding & escaping
   f="$(basename $i)"
-  echo '<li><a href="'$f'">'$f'</a></li>' >> "$o"
+  echo '<li><a href="'"$f$suffix"'">'"$f"'</a></li>' >> "$o"
 done
 
 echo "</ul></body></html>" >> "$o"
